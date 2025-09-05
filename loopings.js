d5f889e57ca99c4cb43c2135994ecc389d4a8614
Jillian Dias <144184711+jijo05@users.noreply.github.com>
2025-09-05 14:35:05 -0300

    Create loopings.js

---
+++ loopings.js
@@ -0,0 +1,37 @@
+/*
+var num = prompt('Digite número para contagem regressiva:')
+while(num >= 0){
+    console.log(`Contador ${num}`)
+    num--
+}
+*/
+/*
+var num1 = prompt('Digite um número máximo para contagem progressiva:')
+var contador = 0
+do{
+    console.log(`Contador ${num1}`)
+    num1++
+} while(num1 >= contador )
+*/
+
+/*
+var d = 1
+while(d <= 10){
+    console.log(`Contador ${d}`)
+    d++
+}
+*/
+/*
+var a = 20
+while (a >= 0) {
+    console.log(`Contador ${a}`)
+    a--
+}
+*/
+
+/*var b = 10
+do {
+    console.log(`Contador ${b}`)
+    b--
+} while(b >= 0)
+*/
